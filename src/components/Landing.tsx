cd332de57a41c74061f0009b32e1043f2ac424e2
theishanbh <theishanbh@gmail.com>
2024-05-28 14:45:32 +0530

    apostrophe fixed

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -21,7 +21,7 @@ function Landing() {
     <div className=" flex flex-col md:flex-row justify-center items-center mb-24">
       <div className="flex flex-col ml-10 md:p-0 flex-[0.6] sm:mb-40">
         <h1 className=" mt-10 md:mt-36 font-extrabold text-4xl md:text-5xl text-cyan-400 ">
-          Hi there, I'm{" "}
+          Hi there, I&apos;m{" "}
         </h1>
         <h2 className=" text-6xl md:text-7xl font-extrabold pb-4">
           Ishan Bhardwaj
